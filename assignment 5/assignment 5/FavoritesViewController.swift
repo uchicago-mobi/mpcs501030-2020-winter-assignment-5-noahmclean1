7111379f8514534e07d711345113a91a1c3cfdaa
Noah McLean <nmclean@lapras.cs.uchicago.edu>
2020-02-09 14:47:17 -0600

    automatically adds plist data on boot, looks kinda pretty

---
+++ assignment 5/assignment 5/FavoritesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavoritesViewController.swift
+//  assignment 5
+//
+//  Created by Noah McLean on 2/9/20.
+//  Copyright © 2020 Noah McLean. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
